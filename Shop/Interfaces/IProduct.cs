530660ca626f9cf74fc192ae4859a7d5b0ea916e
FM-I <ubrfmi@gmail.com>
2021-07-04 10:30:11 +0300

    Добавленно классы Product, ShowCase и интерфейс IProduct (реализованый для класа Product).

---
+++ Shop/Interfaces/IProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Shop.Interfaces
+{
+    /// <summary>
+    /// Интерфейс для описаниия реализации продукта.
+    /// </summary>
+    interface IProduct
+    {
+        /// <summary>
+        /// Идентификатор продукта.
+        /// </summary>
+        int Id { get; }
+        /// <summary>
+        /// Название продукта.
+        /// </summary>
+        string Name { get; }
+        /// <summary>
+        /// Занимаемый обём продуктом.
+        /// </summary>
+        double OccupiedVolume { get; }
+        /// <summary>
+        /// Цена продукта.
+        /// </summary>
+        decimal Price { get; }
+    }
+}
---
+++ Shop/Product.cs
@@ -0,0 +1,49 @@
+﻿using Shop.Interfaces;
+using System;
+
+namespace Shop
+{
+    /// <summary>
+    /// Класс представляющий продукт.
+    /// </summary>
+    public class Product : IProduct
+    {
+        /// <summary>
+        /// Идентификатор продукта.
+        /// </summary>
+        public int Id { get; }
+        /// <summary>
+        /// Название продукта.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Занимаемый обём продуктом.
+        /// </summary>
+        public double OccupiedVolume { get; }
+        /// <summary>
+        /// Цена продукта.
+        /// </summary>
+        public decimal Price { get; }
+
+        public Product(int id, string name, double occupiedVolume, decimal price)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException($"{nameof(name)} не может быть пустим или null.",nameof(name));
+
+            if (id < 0)
+                throw new ArgumentException(nameof(id));
+
+            if (occupiedVolume < 1)
+                throw new ArgumentException(nameof(occupiedVolume));
+
+            if (price < 0)
+                throw new ArgumentException(nameof(price));
+
+
+            Id = id;
+            Name = name;
+            OccupiedVolume = occupiedVolume;
+            Price = price;
+        }
+    }
+}
